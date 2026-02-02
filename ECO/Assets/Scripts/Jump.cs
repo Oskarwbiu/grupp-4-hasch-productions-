46315780307f2051563d0665e8d8267dd82db7b4
Oskarwbiu <oskar.ivarsson@elev.ga.lbs.se>
2026-02-02 11:20:11 +0100

    Jump

---
+++ ECO/Assets/Scripts/Jump.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [SerializeField] float jumpForce = 10;
+    [SerializeField] float jumpCooldown = 0.04f;
+    [SerializeField] ContactFilter2D groundFilter;
+    Rigidbody2D rb;
+    bool isGrounded;
+    bool hasJumped;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void OnJump()
+    {
+        hasJumped = true;
+        Invoke("JumpTimer", jumpCooldown);
+    }
+    void JumpTimer()
+    {
+        hasJumped = false;
+    }
+    void Jump()
+    {   
+       rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+         
+    }
+    private void FixedUpdate()
+    {
+        if (isGrounded && hasJumped)
+        { 
+        Jump();
+            hasJumped = false;
+        }
+        isGrounded = rb.IsTouching(groundFilter);
+    }
+
+}
