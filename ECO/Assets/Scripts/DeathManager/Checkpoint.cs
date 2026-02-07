10293eea4e882f221aed561c23712614bd24e57d
Christopher-Jerrick <christopher-jerrick.bengtsson@elev.ga.lbs.se>
2026-02-07 21:24:08 +0100

    CJ dEATH

---
+++ ECO/Assets/Scripts/DeathManager/Checkpoint.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public static Vector3 lastCheckpointPosition = Vector3.zero;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            lastCheckpointPosition = transform.position;
+            Debug.Log("Checkpoint reached at: " + lastCheckpointPosition);
+        }
+    }
+}
