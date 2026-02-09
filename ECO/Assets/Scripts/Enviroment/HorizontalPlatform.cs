e5aacaa393589d5e05c7ff96f2d7fc64c324b055
Christopher-Jerrick <christopher-jerrick.bengtsson@elev.ga.lbs.se>
2026-02-09 13:18:11 +0100

    ja du

--- ECO/Assets/Scripts/Enviroment/HorizontalPlatform.cs
+++ ECO/Assets/Scripts/Enviroment/HorizontalPlatform.cs
@@ -3,36 +3,55 @@ using UnityEngine;
 public class HorizontalPlatform : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 3f;
-    [SerializeField] private float leftBound = 0f;
-    [SerializeField] private float rightBound = 10f;
+    [SerializeField] private float distance = 5f;
 
-    private Vector3 originalPosition;
+    private Vector3 startPosition;
     private bool movingRight = true;
+    private Vector3 lastPosition;
+    private Rigidbody2D playerOnPlatform;
 
     void Start()
     {
-        originalPosition = transform.position;
-        leftBound = originalPosition.x - 5f;
-        rightBound = originalPosition.x + 5f;
+        startPosition = transform.position;
+        lastPosition = transform.position;
     }
 
     void FixedUpdate()
     {
-        if (movingRight)
+        Vector3 targetPosition = movingRight ?
+            startPosition + Vector3.right * distance :
+            startPosition - Vector3.right * distance;
+
+        if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
         {
-            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
-            if (transform.position.x >= rightBound)
-            {
-                movingRight = false;
-            }
+            movingRight = !movingRight;
         }
-        else
+
+        Vector3 moveDirection = (targetPosition - transform.position).normalized;
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        if (playerOnPlatform != null)
         {
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
-            if (transform.position.x <= leftBound)
-            {
-                movingRight = true;
-            }
+            Vector3 platformMovement = transform.position - lastPosition;
+            playerOnPlatform.transform.position += platformMovement;
+        }
+
+        lastPosition = transform.position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerOnPlatform = collision.gameObject.GetComponent<Rigidbody2D>();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerOnPlatform = null;
         }
     }
 }
