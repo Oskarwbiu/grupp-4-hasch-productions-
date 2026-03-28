2ed76543bdb4c47cd87ce2b16422a8f4b5fb0b3e
HogoDaHogo <hugokleber09@gmail.com>
2026-03-28 16:25:12 +0100

    sewers + grapple animatons

--- ECO/Assets/Scripts/Player/LineController.cs
+++ ECO/Assets/Scripts/Player/LineController.cs
@@ -6,10 +6,11 @@ public class LineController : MonoBehaviour
 
     [SerializeField] private Texture2D[] textures;
     [SerializeField] private float fps;
+    [SerializeField] private bool isreapeating = true;
 
     private int animationStep;
     private float fpsCounter;
-
+    bool stopAnimation = false;
 
     private void Start()
     {
@@ -18,14 +19,26 @@ public class LineController : MonoBehaviour
 
     private void Update()
     {
+        if (!lr.enabled) 
+        { 
+            stopAnimation = false;
+            animationStep = 0;
+            return; 
+        }
+        if (stopAnimation) { return; }
         fpsCounter += Time.deltaTime;
 
         if (fpsCounter >= 1f / fps)
         {
             animationStep++;
-
-            if (animationStep == textures.Length)
+            Debug.Log("step");
+            if (animationStep == textures.Length && isreapeating)
             { animationStep = 0; }
+            else if (animationStep == textures.Length && !isreapeating)
+            {
+                stopAnimation = true;
+                return;
+            }
 
             lr.material.SetTexture("_MainTex", textures[animationStep]);
 
