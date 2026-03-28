ddb53a37fa2bf703b2f4968dfcd062a3f8dd2544
HogoDaHogo <hugokleber09@gmail.com>
2026-03-28 15:49:40 +0100

    Sewrs done and light on grapple point

--- ECO/Assets/Scripts/Enviroment/GrappleFlower.cs
+++ ECO/Assets/Scripts/Enviroment/GrappleFlower.cs
@@ -1,32 +1,47 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class GrappleFlower : MonoBehaviour
 {
     Animator ani;
+    Light2D spotLight;
 
+
+    float startvalue;
+    float endvalue;
     private void Start()
     {
         ani = GetComponent<Animator>();
-        Debug.Log("started");
+        spotLight = GetComponent<Light2D>();
+        spotLight.intensity = 0f;
+
+    }
+
+    private void Update()
+    {
+        float intensity = Mathf.Lerp(startvalue, endvalue, Mathf.PingPong(Time.time, 1));
+        spotLight.intensity = intensity;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Open");
+
         if (collision.gameObject.CompareTag("Indicator"))
         {
             ani.SetTrigger("Open");
-            
+            startvalue = 0.5f;
+            endvalue = 1.5f;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        Debug.Log("Close");
+
         if (collision.gameObject.CompareTag("Indicator"))
         {
             ani.SetTrigger("Close");
-           
+            startvalue = 0;
+            endvalue = 0;
         }
     }
 
